39fb2e06d56c6a694bd2010eac773f98a0a46e41
BillMills <mills.wj@gmail.com>
2015-09-16 14:51:10 -0700

    first cut at demo

--- auxData.js
+++ auxData.js
@@ -1,5 +1,23 @@
 function ulAuxilaryData(route, data){
-	if(route == "{{species}}/{{A}}")
-		return {q: [0,1,2,3,4,5,6]}
+
+	var url, path, i;
+
+	url = window.location.protocol + "//" + window.location.host + "/" 
+	path = window.location.pathname.split('/').slice(0,-1);
+	for(i=0; i<path.length; i++){
+		url += path[i] + '/'
+	}
+
+
+	if(route == "{{cats}}")
+		return {'images': 
+			[
+				url+'img/pix1',
+				url+'img/pix2',
+				url+'img/pix3',
+				url+'img/pix4',
+				url+'img/pix5'
+			]
+		}
 	return {}
 }
