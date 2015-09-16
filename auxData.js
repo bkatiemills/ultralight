21c000f1d568f0b882059ac1a82eca757523e3ed
BillMills <mills.wj@gmail.com>
2015-09-16 15:10:58 -0700

    first demo

--- auxData.js
+++ auxData.js
@@ -1,23 +1,21 @@
 function ulAuxilaryData(route, data){
 
-	var url, path, i;
+	var url, path, i, pics,
+	numcats = parseInt(data['cats']);
 
+	//extract the base URL where this page is found.
 	url = window.location.protocol + "//" + window.location.host + "/" 
 	path = window.location.pathname.split('/').slice(0,-1);
 	for(i=0; i<path.length; i++){
 		url += path[i] + '/'
 	}
 
-
-	if(route == "{{cats}}")
-		return {'images': 
-			[
-				url+'img/pix1.jpg',
-				url+'img/pix2.jpg',
-				url+'img/pix3.jpg',
-				url+'img/pix4.jpg',
-				url+'img/pix5.jpg'
-			]
+	if(route == "{{cats}}"){
+		pics = []
+		for(i=0; i<numcats; i++){
+			pics[pics.length] = url+'img/pix'+(i+1)+'.jpg';
 		}
+		return {'images': pics}
+	}
 	return {}
 }
